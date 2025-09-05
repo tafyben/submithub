ef4277a9d7442f41fc4fd1e3e387171f3b9e8838
Tafadzwa Ben <tbmukwepa@gmail.com>
2025-09-05 15:03:24 +0200

    initial commit

---
+++ database/migrations/2025_09_05_130056_create_assignments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('file_path')->nullable();
+            $table->timestamp('submitted_at')->nullable();
+            $table->enum('status', ['pending', 'submitted', 'graded'])->default('pending');
+            $table->string('grade')->nullable();
+            $table->text('feedback')->nullable();
+            $table->timestamps();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assignments');
+    }
+};
